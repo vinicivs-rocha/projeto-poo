514085ffad75cd007d86a48737bbf787cf040d61
Vinícius Rocha <viniciusroch65@gmail.com>
2023-05-13 20:14:59 -0300

    Product detail modal

---
+++ static/js/modal.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector('#modalAtualizacao')
+const removeButton = document.querySelector('#remove-button')
+let modalImage = document.querySelector('#modal-image')
+let modalNameInput = document.querySelector('#modal-name')
+let modalPriceInput = document.querySelector('#modal-price')
+let modalQuantityInput = document.querySelector('#modal-quantity')
+
+modal.addEventListener('show.bs.modal', ({ relatedTarget }) => {
+    let { image, name, price, quantity, id } = relatedTarget.dataset
+
+    modalImage.setAttribute('src', image)
+    modalNameInput.value = name
+    modalPriceInput.value = price
+    modalQuantityInput.value = quantity
+    removeButton.dataset.id = id
+})
+
+removeButton.addEventListener('click', ({ target }) => {
+    let { id } = target.dataset
+
+    fetch(`url/${id}`)
+})
+
