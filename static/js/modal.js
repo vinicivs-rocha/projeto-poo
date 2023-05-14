4dc128aec22a09134e629f449c6a69569772bdb2
Vinícius Rocha <viniciusroch65@gmail.com>
2023-05-14 14:17:56 -0300

    Add product's modal and remove product's page added

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,11 +1,13 @@
-const modal = document.querySelector('#modalAtualizacao')
+const modalAtualizacao = document.querySelector('#modalAtualizacao')
+const modalCriar = document.querySelector('#modal-criar')
 const removeButton = document.querySelector('#remove-button')
 let modalImage = document.querySelector('#modal-image')
+let modalCriarImage = document.querySelector('#modal-criar-image')
 let modalNameInput = document.querySelector('#modal-name')
 let modalPriceInput = document.querySelector('#modal-price')
 let modalQuantityInput = document.querySelector('#modal-quantity')
 
-modal.addEventListener('show.bs.modal', ({ relatedTarget }) => {
+modalAtualizacao.addEventListener('show.bs.modal', ({ relatedTarget }) => {
     let { image, name, price, quantity, id } = relatedTarget.dataset
 
     modalImage.setAttribute('src', image)
@@ -13,5 +15,11 @@ modal.addEventListener('show.bs.modal', ({ relatedTarget }) => {
     modalPriceInput.value = price
     modalQuantityInput.value = quantity
     removeButton.setAttribute('href', `url/${id}`)
+})
+
+modalCriar.addEventListener('show.bs.modal', ({ relatedTarget }) => {
+    let { image } = relatedTarget.dataset
+
+    modalCriarImage.setAttribute('src', image)
 })
 
