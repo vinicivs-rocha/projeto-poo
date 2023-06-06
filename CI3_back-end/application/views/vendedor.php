3fdac018318783bb7893cbc2e3a17c87517dc3f6
vinicivs-rocha <viniciusroch65@gmail.com>
2023-06-06 09:27:59 -0300

    Vendedor JQuery added

--- CI3_back-end/application/views/vendedor.php
+++ CI3_back-end/application/views/vendedor.php
@@ -63,7 +63,7 @@
         </div>
     </header>
     <main class="container">
-        <div class="row g-3">
+        <div class="row g-3" id="container-row">
             <div class="col-4">
                 <div class="card shadow">
                     <div class="card-body">
@@ -123,12 +123,14 @@
                     </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="/pedido" method="post">
+                <!-- Adicionar rota de adição do produto ao pedido criado -->
+                <form action="" method="post">
                     <div class="modal-body">
                         <div class="row">
                             <div class="col">
+                                <input type="number" name="id" id="id" class="d-none">
                                 <label for="name" class="form-label">Nome</label>
-                                <input type="text" name="name" id="name" class="form-control" disabled readonly />
+                                <input type="text" name="nome_produto" id="name" class="form-control" disabled readonly />
                             </div>
                         </div>
                         <div class="row">
@@ -136,7 +138,7 @@
                                 <label for="price" class="form-label">Preço</label>
                                 <div class="input-group">
                                     <span class="input-group-text">R$</span>
-                                    <input type="number" name="price" id="price" class="form-control" step="0.01" />
+                                    <input type="number" name="preco" id="price" class="form-control" step="0.01" />
                                 </div>
                             </div>
                         </div>
@@ -144,7 +146,7 @@
                             <div class="col">
                                 <label for="quantity" class="form-label">Quantidade</label>
                                 <div class="input-group">
-                                    <input type="number" name="quantity" id="quantity" class="form-control" />
+                                    <input type="number" name="estoque" id="quantity" class="form-control" />
                                     <span class="input-group-text">unid.</span>
                                 </div>
                             </div>
@@ -166,36 +168,43 @@
                     <h5 class="modal-title">Cadastrar cliente</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="/cliente" method="post">
+                <!-- Adicionar rota de criação de cliente -->
+                <form action="/" method="post">
                     <div class="modal-body">
                         <div class="row g-3 mb-3">
                             <div class="col-8">
                                 <label for="client-name" class="form-label">Nome:</label>
-                                <input type="text" name="client-name" id="client-name" class="form-control" />
+                                <input type="text" name="nome" id="client-name" class="form-control" />
                             </div>
                             <div class="col-4">
                                 <label for="client-gender" class="form-label">Gênero:</label>
-                                <input type="text" name="client-gendet" id="client-gender" class="form-control" />
+                                <input type="text" name="genero" id="client-gender" class="form-control" />
                             </div>
                         </div>
                         <div class="row g-3 justify-content-start mb-3">
                             <div class="col-2">
                                 <label for="client-age" class="form-label">Idade:</label>
-                                <input type="number" name="client-age" id="client-age" class="form-control" />
+                                <input type="number" name="idade" id="client-age" class="form-control" />
                             </div>
                             <div class="col-4">
                                 <label for="client-phone" class="form-label">Telefone:</label>
-                                <input type="tel" name="client-phone" id="client-phone" class="form-control" />
+                                <input type="tel" name="telefone" id="client-phone" class="form-control" />
                             </div>
                         </div>
-                        <div class="row g-3 justify-content-start">
+                        <div class="row g-3 justify-content-start mb-3">
                             <div class="col-4">
                                 <label for="client-cpf" class="form-label">CPF:</label>
-                                <input type="text" name="client-cpf" id="client-cpf" class="form-control" />
+                                <input type="text" name="cpf" id="client-cpf" class="form-control" />
                             </div>
                             <div class="col-6">
                                 <label for="client-email" class="form-label">Email:</label>
-                                <input type="email" name="clien-email" id="client-email" class="form-control" />
+                                <input type="email" name="email" id="client-email" class="form-control" />
+                            </div>
+                        </div>
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-12">
+                                <label for="client-address" class="form-label">Endereço:</label>
+                                <input type="text" name="endereco" id="client-address" class="form-control">
                             </div>
                         </div>
                     </div>
@@ -209,16 +218,56 @@
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script>
+        $(document).ready(buscarProdutos)
+
+        let containerRow = $("#container-row")
+
+        function buscarProdutos() {
+            $.ajax({
+                url: '<?= base_url('estoquista/pesquisa_produtos') ?>',
+                type: "GET",
+                dataType: "JSON",
+                cache: false,
+                success: montarProdutos
+            })
+        }
+
+        function montarProdutos(data) {
+            data.forEach(product => {
+                containerRow.append(`
+                    <div class="col-4">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">
+                                    ${product.nome_produto}
+                                </h5>
+                            </div>
+                            <img src="${product.image}" alt="${product.nome_produto}" class="card-img-bottom" />
+                            <div class="card-footer d-flex flex-row justify-content-end">
+                                <button type="button" class="btn btn-light" data-id="${product.id}" data-name="${product.nome_produto}" data-price="${product.preco}" data-id="${product.id}" data-bs-toggle="modal" data-bs-target="#add-product-modal">
+                                    <i class="fi fi-rr-cart-arrow-down fi-add-cart d-flex"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+            `)
+            });
+        }
+    </script>
     <script>
         const addProductModal = document.querySelector('#add-product-modal')
         const modalNameInput = document.querySelector('#name')
         const modalPriceInput = document.querySelector('#price')
+        const modalIdInput = document.querySelector('#id')
 
         addProductModal.addEventListener('shown.bs.modal', ({
             relatedTarget
         }) => {
             modalNameInput.value = relatedTarget.dataset.name
             modalPriceInput.value = relatedTarget.dataset.price
+            modalIdInput = relatedTarget.dataset.id
         })
     </script>
 </body>
