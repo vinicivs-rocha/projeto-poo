8275ba7288593476feff18c592581ce19887b474
Cavalcant1DEV <Justplaytoplay@gmail.com>
2023-06-06 14:28:13 -0300

    Update pedidos.php

--- CI3_back-end/application/views/pedidos.php
+++ CI3_back-end/application/views/pedidos.php
@@ -57,151 +57,7 @@
 			</div>
 		</header>
 		<main class="container-xxl mt-5" id="container">
-			<div class="row mb-5">
-				<div class="card p-0">
-					<div class="card-header d-flex flex-row justify-content-between">
-						<h6 class="mb-0">Vendido por <strong>José Roberto</strong></h6>
-						<h6 class="mb-0">R$ 118,00</h6>
-					</div>
-					<div class="card-body">
-						<h5 class="card-title">Pedido 227</h5>
-						<h6 class="card-subtitle text-danger">Aguardando confirmação</h6>
-					</div>
-					<ul class="list-group list-group-flush">
-						<li class="list-group-item">José Edilson Pereira Silva</li>
-						<li class="list-group-item">123.456.789-10</li>
-						<li class="list-group-item">Francisco Otaviano, 145</li>
-					</ul>
-					<div class="card-footer d-flex flex-row justify-content-end">
-						<button
-							type="button"
-							class="btn btn-light btn-pedido"
-							data-bs-toggle="modal"
-							data-bs-target="#pedido-modal-1"
-						>
-							Resumo de itens
-						</button>
-					</div>
-				</div>
-			</div>
-			<div class="row mb-5">
-				<div class="card p-0">
-					<div class="card-header d-flex flex-row justify-content-between">
-						<h6 class="mb-0">Vendido por <strong>José Roberto</strong></h6>
-						<h6 class="mb-0">R$ 118,00</h6>
-					</div>
-					<div class="card-body">
-						<h5 class="card-title">Pedido 228</h5>
-						<h6 class="card-subtitle text-success">Confirmado</h6>
-					</div>
-					<ul class="list-group list-group-flush">
-						<li class="list-group-item">José Edilson Pereira Silva</li>
-						<li class="list-group-item">123.456.789-10</li>
-						<li class="list-group-item">Francisco Otaviano, 145</li>
-					</ul>
-					<div class="card-footer d-flex flex-row justify-content-end">
-						<button
-							type="button"
-							class="btn btn-light btn-pedido"
-							data-bs-toggle="modal"
-							data-bs-target="#pedido-modal-2"
-						>
-							Resumo de itens
-						</button>
-					</div>
-				</div>
-			</div>
 		</main>
-		<!-- Gerar um modal para cada pedido -->
-		<div id="pedido-modal-1" class="modal" tabindex="-1">
-			<div class="modal-dialog">
-				<form action="#" method="post">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title">Pedido 227</h5>
-							<button
-								type="button"
-								class="btn-close"
-								data-bs-dismiss="modal"
-								aria-label="Close"
-							></button>
-						</div>
-						<div class="modal-body">
-							<table class="table table-hover table-bordered align-middle">
-								<thead>
-									<tr>
-										<th scope="col">Nome</th>
-										<th scope="col">Quantidade</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td>
-											Argamassa AC I Cerâmica Uso Interno Kerakoll Cimentolit
-											20Kg
-										</td>
-										<td class="text-center">1</td>
-									</tr>
-									<tr>
-										<td>
-											Argamassa para Assentamento de Porcelanato Interno E
-											Externo 20kg Branca
-										</td>
-										<td class="text-center">2</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<div class="modal-footer justify-content-center">
-							<button type="submit" class="btn btn-light btn-pedido">
-								Confirmar
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-		<!-- Se o pedido tiver status confirmado o modal n possui form nem botão de confirmação -->
-		<div id="pedido-modal-2" class="modal" tabindex="-1">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">Pedido 227</h5>
-						<button
-							type="button"
-							class="btn-close"
-							data-bs-dismiss="modal"
-							aria-label="Close"
-						></button>
-					</div>
-					<div class="modal-body">
-						<table class="table table-hover table-bordered align-middle">
-							<thead>
-								<tr>
-									<th scope="col">Nome</th>
-									<th scope="col">Quantidade</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>
-										Argamassa AC I Cerâmica Uso Interno Kerakoll Cimentolit 20Kg
-									</td>
-									<td class="text-center">1</td>
-								</tr>
-								<tr>
-									<td>
-										Argamassa para Assentamento de Porcelanato Interno E Externo
-										20kg Branca
-									</td>
-									<td class="text-center">2</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
 		<script
 			src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
 			integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
@@ -258,7 +114,7 @@
 			                </div>
 			            </div>
 			        `)
-			        buscarListaProdutos(order.id, order.status_pedido_id, index)
+			        buscarListaProdutos(order.id_pedido, order.status_pedido_id, index)
 			    })
 			}
 
@@ -271,6 +127,7 @@
 					cache: false,
 			        data: {id},
 			        success: (data) => {
+                        console.log(data,id,status,index);
                         montarModalProdutos(data, id, status, index)
                     }
 			    });
